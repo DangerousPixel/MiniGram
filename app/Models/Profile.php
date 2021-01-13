c1fab58ae85f45ce2392df6f0b33bd0830c5024b
DangerousPixel <siidrove@gmail.com>
2021-01-14 02:02:06 +0300

    default image for new users

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -9,7 +9,7 @@ class Profile extends Model
 
     public function profileImage()
     {
-        $imagePath = ($this->image) ? $this->image : '/profile/InnESoG2N1pQVRhTzs4z1xwkKrpy3irdYKFrYQJN.png';
+        $imagePath = ($this->image) ? $this->image : '/profile/SgseEcsCaTTdfulfNmEGd39IE9bz6VQ4ftXLR0BV.png';
         return '/storage/' . $imagePath ;
     }
 
